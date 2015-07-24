7a7bc72b9dbef4c00b8a1f3f741dc8d43fc1630b
Jenny Olsen <jenny.a.olsen@gmail.com>
2015-07-24 13:56:57 -0700

    finish chapter 6

---
+++ app/scripts/views/song_view.js
@@ -0,0 +1,11 @@
+RocknrollcallYeoman.SongView = Em.View.extend({
+  didInsertElement: function() {
+    var duration = $('#song-duration').text(function(){
+      var $this = $(this);
+      var origSeconds = $this.attr('data-duration');
+      var minutes = Math.floor(origSeconds/60);
+      var seconds = Math.floor(origSeconds % 60);
+      return minutes + ":" + seconds
+    });
+  }
+});
