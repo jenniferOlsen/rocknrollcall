657cb35d6c4eca2bf50f48a06223f2c54a318da9
Jenny Olsen <jenny.a.olsen@gmail.com>
2015-07-24 13:40:14 -0700

    working app

--- app/scripts/routes/song_route.js
+++ app/scripts/routes/song_route.js
@@ -1,12 +1,26 @@
-RocknrollcallYeoman.Song = Em.Object.extend({
-  id: null,
-  enid: null,
-  title: null,
-  artist_name: null,
-  artist_id: null,
-  audio_md5: null,
-  audio_summary: null,
-  hotttnesss: null,
-  track: null,
-  types: null
+RocknrollcallYeoman.SongRoute = Ember.Route.extend({
+  model: function(params) {
+
+    //find the song byId
+    var url = "http://developer.echonest.com/api/v4/song/profile?api_key=LEGFCAZCQ8QFGDXYK&format=json&bucket=audio_summary&bucket=song_hotttnesss&bucket=tracks&bucket=song_type&bucket=id:7digital-US", obj = {"id": params.enid}
+
+    return Ember.$.getJSON(url, obj)
+      //returns Promise object
+        .then(function(data) {
+        var entry = data.response.songs[0];
+        var track = null;
+        if (entry.tracks.length) track = entry.tracks[0];
+
+        return RocknrollcallYeoman.Song.create({
+          enid: entry.id,
+          title: entry.title,
+          hotttnesss: entry.song_hotttnesss,
+          track: track,
+          types: entry.song_type,
+          audio_summary: entry.audio_summary,
+          artist_id: entry.artist_id,
+          artist_name: entry.artist_name
+        });
+      });
+  }
 });
