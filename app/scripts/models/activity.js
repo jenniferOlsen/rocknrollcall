1a01e67e99d86d322eb617982a27ea28d07e6df4
Jenny Olsen <jenny.a.olsen@gmail.com>
2015-07-27 09:46:23 -0700

    hard-code some temporary data for model for development

---
+++ app/scripts/models/activity.js
@@ -0,0 +1,22 @@
+RocknrollcallYeoman.Activity = DS.Model.extend({
+  display_id: DS.attr('string'),
+  type: DS.attr('string'),
+  display_name: DS.attr('string'),
+  hotttnesss: DS.attr('number'),
+  timestamp: DS.attr()
+});
+RocknrollcallYeoman.Activity.FIXTURES = [{
+  id: 0,
+  display_id: "Activity1",
+  type: "song",
+  display_name: "On the Road Again",
+  hotttnesss: 54,
+  timestamp: "Fri Dec 06 2013 01:05:53 GMT-0600 (CST)"
+}, {
+  id: 1,
+  display_id: "Activity2",
+  type: "artist",
+  display_name: "Willie Nelson",
+  hotttnesss: 99,
+  timestamp: "Fri Dec 06 2013 01:05:53 GMT-0600 (CST)"
+}];
